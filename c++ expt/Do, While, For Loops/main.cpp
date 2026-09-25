4b2be0d081998e436fbb060148d18b10243e3036
Azka <287875000+Derilkarf@users.noreply.github.com>
2026-09-25 12:53:01 +0700

    Refactor code structure for improved readability and maintainability

---
+++ c++ expt/Do, While, For Loops/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() {
+        // while loop
+
+    /* 
+        int battery = 30;
+
+        while (battery < 100) {
+            std::cout << "Charging.... " << battery << "%\n";
+            battery += 10; // will trigger infinite loop if this was remove
+        }
+        std::cout << "Battery Full! " << battery << "%\n";
+    
+
+        // do while loop = do some program first
+        //                 And then repeat again if condition is true
+
+    
+        int number = 0; //will trigger infinite loop if the value is the same as the condition
+
+        do{
+            std::cout << number << '\n';
+            number++;
+        } while(number <= 5);
+    
+
+        // for loop
+
+        for(int index = 10; index >= 0; index--) {
+            std::cout << index << '\n';
+        } // if you change the increment, while the value is the same, it will trigger infinite loop.
+
+        std::cout << "OOOOO MY PCCCCCCCCCC!!!\n";
+    */
+    return 0;
+}
+
